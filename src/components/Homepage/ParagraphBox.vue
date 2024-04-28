559d1b8a097912388fa35e5a744f134d217dcda8
Val <pro.mvaldo@gmail.com>
2024-04-28 10:35:23 +0200

    🎨 prettier

---
+++ src/components/Homepage/ParagraphBox.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const sentence = 'Hello World lorem ipsum'
+
+const sentenceWord = computed(() => sentence.split(' '))
+</script>
+
+<template>
+  <div class="word-wrapper">
+    <div v-for="(word, index) in sentenceWord" :key="index">{{ word }}</div>
+  </div>
+</template>
+
+<style scoped>
+.word-wrapper {
+  display: flex;
+  gap: 8px;
+  color: white;
+  font-size: x-large;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ParagraphBox from '@/components/Homepage/ParagraphBox.vue'
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <ParagraphBox></ParagraphBox>
+  </main>
 </template>
+
+<style scoped>
+main {
+  background-color: black;
+  min-height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
